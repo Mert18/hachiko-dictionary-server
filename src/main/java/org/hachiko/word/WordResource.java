88af029502ea5be1133e04538c883aa50b8f09a2
mert18 <mertuygur02@gmail.com>
2022-07-08 01:34:23 +0300

    ssssh

--- src/main/java/org/hachiko/word/WordResource.java
+++ src/main/java/org/hachiko/word/WordResource.java
@@ -8,11 +8,9 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -27,21 +25,18 @@ public class WordResource {
   WordRepository wordRepository;
 
   @POST
+  @Produces(MediaType.APPLICATION_JSON)
+  @Consumes(MediaType.APPLICATION_JSON)
   public Response create(Word w) {
     wordRepository.persist(w);
     return Response.created(URI.create("/words/" + w.id.toString())).build();
   }
 
   @GET
+  @Produces(MediaType.APPLICATION_JSON)
   @Path("{id}")
   public Word read(@PathParam("id") String id) {
     return wordRepository.findById(new ObjectId(id));
-  }
-
-  @PUT
-  @Path("{id}")
-  public void update(Word w) {
-    wordRepository.update(w);
   }
 
   @DELETE
@@ -51,13 +46,9 @@ public class WordResource {
   }
 
   @GET
+  @Produces(MediaType.APPLICATION_JSON)
   public List<Word> list() {
     return wordRepository.listAll();
   }
 
-  @GET
-  @Path("search")
-  public List<Word> search(@QueryParam("title") String title) {
-    return wordRepository.list("title", title);
-  }
 }
