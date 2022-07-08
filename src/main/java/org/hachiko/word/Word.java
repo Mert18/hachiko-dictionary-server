64af1795bcc8d04081effa098940c020414d3a2b
mert18 <mertuygur02@gmail.com>
2022-07-08 23:31:49 +0300

    feat: Documentation swagger-ui added

--- src/main/java/org/hachiko/word/Word.java
+++ src/main/java/org/hachiko/word/Word.java
@@ -5,11 +5,12 @@ import java.util.Set;
 import org.bson.codecs.pojo.annotations.BsonId;
 import org.bson.codecs.pojo.annotations.BsonProperty;
 import org.bson.types.ObjectId;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 import io.quarkus.mongodb.panache.PanacheMongoEntityBase;
 
+@Schema(name = "Word", description = " WOrd representation")
 public class Word extends PanacheMongoEntityBase {
-
   @BsonProperty("_id")
   @BsonId
   public ObjectId id;
--- src/main/java/org/hachiko/word/WordResource.java
+++ src/main/java/org/hachiko/word/WordResource.java
@@ -15,10 +15,17 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.bson.types.ObjectId;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 @Path("/words")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
+@Tag(name = "Word Resource", description = "Word REST APIs")
 public class WordResource {
 
   @Inject
@@ -27,7 +34,10 @@ public class WordResource {
   @POST
   @Produces(MediaType.APPLICATION_JSON)
   @Consumes(MediaType.APPLICATION_JSON)
+  @Operation(operationId = "createWord", summary = "Create a word", description = "Used for creating word from a specific syntax.")
+  @APIResponse(responseCode = "201", description = "Operation completed", content = @Content(mediaType = MediaType.APPLICATION_JSON))
   public Response create(Word w) {
+
     wordRepository.persist(w);
     return Response.created(URI.create("/words/" + w.id.toString())).build();
   }
@@ -35,18 +45,25 @@ public class WordResource {
   @GET
   @Produces(MediaType.APPLICATION_JSON)
   @Path("{id}")
+  @Operation(operationId = "getWord", summary = "Fetch a word", description = "Fetch a word by its id.")
+  @APIResponse(responseCode = "200", description = "Operation completed", content = @Content(mediaType = MediaType.APPLICATION_JSON))
   public Word read(@PathParam("id") String id) {
+
     return wordRepository.findById(new ObjectId(id));
   }
 
   @DELETE
   @Path("{id}")
+  @Operation(operationId = "deleteWord", summary = "Delete a word", description = "Delete a word by its id.")
+  @APIResponse(responseCode = "200", description = "Operation completed", content = @Content(mediaType = MediaType.APPLICATION_JSON))
   public void delete(@PathParam("id") String id) {
     wordRepository.findByIdOptional(new ObjectId(id)).ifPresent(w -> wordRepository.delete(w));
   }
 
   @GET
   @Produces(MediaType.APPLICATION_JSON)
+  @Operation(operationId = "getAllWords", summary = "Get all words", description = "Used for fetching all words from the database.")
+  @APIResponse(responseCode = "200", description = "Operation completed", content = @Content(mediaType = MediaType.APPLICATION_JSON))
   public List<Word> list() {
     return wordRepository.listAll();
   }
