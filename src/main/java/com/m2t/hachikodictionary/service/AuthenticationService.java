821646ea4d263473f02e16cd399533b182147a48
mert18 <mertuygur02@gmail.com>
2023-11-05 18:39:18 +0300

    35 - auth refactored, swagger added.

--- src/main/java/com/m2t/hachikodictionary/service/AuthenticationService.java
+++ src/main/java/com/m2t/hachikodictionary/service/AuthenticationService.java
@@ -9,6 +9,8 @@ import com.m2t.hachikodictionary.exception.*;
 import com.m2t.hachikodictionary.model.Account;
 import com.m2t.hachikodictionary.model.Role;
 import com.m2t.hachikodictionary.repository.AccountRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,6 +25,7 @@ public class AuthenticationService {
     private final JWTService jwtService;
     private final AuthenticationManager authenticationManager;
     private final PasswordEncoder passwordEncoder;
+    private static final Logger logger = LoggerFactory.getLogger(AuthenticationService.class);
 
     public AuthenticationService(AccountRepository accountRepository, AccountService accountService,
                                  JWTService jwtService, AuthenticationManager authenticationManager,
@@ -38,6 +41,7 @@ public class AuthenticationService {
     }
 
     public Response register(RegistrationRequest registrationRequest) {
+        // Checks
         if(!registrationRequest.getPassword().equals(registrationRequest.getConfirmPassword())) {
             throw new PasswordsDoNotMatchException("Passwords do not match.");
         }
@@ -48,72 +52,59 @@ public class AuthenticationService {
             throw new EmailAlreadyExistsException("Email already exists.");
         }
 
-        try {
-            Account account = new Account(
-                    registrationRequest.getUsername(),
-                    passwordEncoder.encode(registrationRequest.getPassword()),
-                    registrationRequest.getEmail(),
-                    Role.USER
-            );
+        Account account = new Account(
+                registrationRequest.getUsername(),
+                passwordEncoder.encode(registrationRequest.getPassword()),
+                registrationRequest.getEmail(),
+                Role.USER
+        );
 
-            accountRepository.save(account);
+        // Save to database
+        accountRepository.save(account);
+        logger.info("Account {} saved to database.", account.getUsername());
 
-            AuthenticationResponse authResponse = jwtService.generateToken(account);
+        // Generate token
+        AuthenticationResponse authResponse = jwtService.generateToken(account);
+        String token = confirmationService.create(registrationRequest.getEmail());
 
-            String token = confirmationService.create(registrationRequest.getEmail());
-            mailService.sendConfirmationEmail(registrationRequest, token);
+        // Send email
+        mailService.sendConfirmationEmail(registrationRequest, token);
+        logger.info("Confirmation mail sent to user {}", account.getUsername());
 
-            return new Response(true, "Registration successful.", authResponse);
-        } catch (Exception e) {
-            throw new RuntimeException("Registration failed: " + e.getMessage());
-        }
+        return new Response(true, "Registration successful.", authResponse);
     }
 
-    public Response login(LoginRequest loginRequest) throws Exception {
-        try {
-            Account user = accountService.loadUserByEmail(loginRequest.getEmail());
-            if(!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
-                throw new InvalidCredentialsException("Invalid credentials.");
-            }
-            authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(
-                            user.getUsername(),
-                            loginRequest.getPassword()
-                    )
-            );
-            Account account = accountRepository.findAccountByEmail(loginRequest.getEmail());
-            AuthenticationResponse authResponse = jwtService.generateToken(account);
-            Response response = new Response(true, "Login successful.", authResponse);
-            return response;
-        } catch (AccountNotFoundException e) {
-            throw new AccountNotFoundException("Account not found.");
-        } catch (InvalidCredentialsException e) {
+    public Response login(LoginRequest loginRequest) {
+        // Account existence check.
+        Account account = accountService.loadUserByEmail(loginRequest.getEmail());
+        if(account == null) {
+            throw new AccountNotFoundException("Account not found with email " + loginRequest.getEmail());
+        }
+        // Password check.
+        if(!passwordEncoder.matches(loginRequest.getPassword(), account.getPassword())) {
             throw new InvalidCredentialsException("Invalid credentials.");
-        } catch (Exception e) {
-            System.out.println("exception happened: " + e);
-            throw new Exception("Login failed: " + e.getMessage());
         }
 
+        authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                    account.getUsername(),
+                    loginRequest.getPassword()
+            ));
 
+        AuthenticationResponse authResponse = jwtService.generateToken(account);
+        logger.info("{} login successful.", account.getUsername());
+
+        return new Response(true, "Login successful.", authResponse);
     }
 
     public Response refreshToken(String token) {
-        try {
-            String username = jwtService.extractUsername(token);
-            Account account = accountService.loadUserByUsername(username);
-            if(!jwtService.isTokenValid(token, account)) {
-                throw new InvalidTokenException("Token is invalid.");
-            }
-            AuthenticationResponse authResponse = jwtService.generateToken(account);
-            Response response = new Response(true, "Token refreshed.", authResponse);
-            return response;
+        String username = jwtService.extractUsername(token);
+        Account account = accountService.loadUserByUsername(username);
+        if(!jwtService.isTokenValid(token, account)) {
+            throw new InvalidTokenException("Token could not be validated.");
         }
-        catch (InvalidTokenException e) {
-            throw new InvalidTokenException("Token is invalid.");
-        }
-        catch (Exception e) {
-            throw new RuntimeException("Token refresh failed: " + e.getMessage());
-        }
-
+        AuthenticationResponse authResponse = jwtService.generateToken(account);
+        Response response = new Response(true, "Token refreshed.", authResponse);
+        return response;
     }
 }
